ca0edb310170edcfdbd7e616f5667fe7314a9c94
fujimon1987 <sunegeyosho@gmail.com>
2015-12-11 13:39:48 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-    def show
+  def show
     @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,10 +17,23 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+    params.require(:user).permit(:name, :email, :area, :password,
+                                 :password_confirmation)
   end
 end
