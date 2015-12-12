99fae9542da0ce2799e458b81b9f93d7b74e65cb
fujimon1987 <sunegeyosho@gmail.com>
2015-12-12 07:22:28 +0000

    microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+    belongs_to :user
+    validates :user_id, presence: true
+    validates :content, presence: true, length: { maximum: 140 }
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -6,4 +9,5 @@ class User < ActiveRecord::Base
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
     has_secure_password
+    has_many :microposts
 end
