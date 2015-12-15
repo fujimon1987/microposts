4242bb7094040c67eb534731d10daa759a41f029
fujimon1987 <sunegeyosho@gmail.com>
2015-12-15 11:14:10 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
-
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-　
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params) 
@@ -48,3 +48,4 @@ class UsersController < ApplicationController
       end
   end
 end
+
