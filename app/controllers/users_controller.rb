52bebebc517c2eef96abdf7d490f8b2c39964a6e
fujimon1987 <sunegeyosho@gmail.com>
2015-12-15 10:46:31 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
+　
   def update
     @user = User.find(params[:id])
     if @user.update(user_params) 
